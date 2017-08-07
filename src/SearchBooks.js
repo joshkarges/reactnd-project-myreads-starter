4d53e35f7dcb49010943bb5458dc237edef3bf69
joshua <joshkarges@gmail.com>
2017-08-07 03:04:05 -0400

    cleanup

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -10,7 +10,7 @@ class SearchBooks extends Component {
   }
 
   updateQuery = (query) => {
-    this.setState({ query: query.trim().toLowerCase() })
+    this.setState({ query: query.toLowerCase() })
   }
 
   render() {
@@ -20,7 +20,7 @@ class SearchBooks extends Component {
     let showingBooks
     if (query) {
       const match = new RegExp(escapeRegExp(query), 'i')
-      showingBooks = books.filter((contact) => match.test(contact.title))
+      showingBooks = books.filter((book) => match.test(book.title) || book.authors.some((author) => match.test(author)))
     } else {
       showingBooks = books
     }
@@ -32,16 +32,12 @@ class SearchBooks extends Component {
         <div className="search-books-bar">
           <Link className="close-search" to='/'>Close</Link>
           <div className="search-books-input-wrapper">
-            {/*
-              NOTES: The search from BooksAPI is limited to a particular set of search terms.
-              You can find these search terms here:
-              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-              you don't find a specific author or title. Every search is limited by search terms.
-            */}
-            <input type="text" placeholder="Search by title or author" onChange={(event) => this.updateQuery(event.target.value)}/>
-
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={query}
+              onChange={(event) => this.updateQuery(event.target.value)}
+            />
           </div>
         </div>
         <div className="search-books-results">
